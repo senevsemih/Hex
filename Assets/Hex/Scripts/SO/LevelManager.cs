7e18a4f73573006fd1c7d928e548ce859c43dbaf
senevsemih <senevsemih@gmail.com>
2021-03-17 06:48:58 +0300

    +created LevelManager SO

---
+++ Assets/Hex/Scripts/SO/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Hex.Scripts.SO
+{
+    [CreateAssetMenu(fileName = "Level Manager", menuName = "Hex/Level Settings")]
+    public class LevelManager : ScriptableObject
+    {
+        public void Next()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
