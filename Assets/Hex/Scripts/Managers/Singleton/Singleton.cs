5d1ad656588d151b47c90cee421879a693857631
senevsemih <senevsemih@gmail.com>
2021-03-17 06:51:53 +0300

    +singleton

---
+++ Assets/Hex/Scripts/Managers/Singleton/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Hex.Scripts.Managers.Singleton
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        var obj = new GameObject();
+                        obj.hideFlags = HideFlags.HideAndDontSave;
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+
+            set => _instance = value;
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            { 
+                _instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
