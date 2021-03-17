f39c443d532c599a7a00b35c5cd5943281a85408
senevsemih <senevsemih@gmail.com>
2021-03-17 06:51:14 +0300

    +created dummycontroller

---
+++ Assets/Hex/Scripts/Dummy/DummyRotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Hex.Scripts.Dummy
+{
+    public class DummyRotate : MonoBehaviour
+    {
+        [SerializeField] private float _rotateAngle;
+        
+        private void OnMouseDown()
+        {
+            transform.Rotate( new Vector3(0, _rotateAngle, 0) );
+        }
+    }
+}
---
+++ Assets/Hex/Scripts/Dummy/DummySlide.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Hex.Scripts.Dummy
+{
+    public class DummySlide : MonoBehaviour
+    {
+	    private Vector3 _distance;
+	    private Vector3 _startPosition;
+	    private float _positionX;
+	    private float _positionY;
+	    private float _positionZ;
+	    
+	    private void OnMouseDown()
+	    {
+		    _startPosition = transform.position;
+		    _distance = Camera.main.WorldToScreenPoint(transform.position);
+		    _positionX = Input.mousePosition.x - _distance.x;
+		    _positionY = Input.mousePosition.y - _distance.y;
+		    _positionZ = Input.mousePosition.z - _distance.z;
+	    }
+ 
+	    private void OnMouseDrag()
+	    {
+		    float distanceX = Input.mousePosition.x - _positionX;
+		    float distanceY = Input.mousePosition.y - _positionY;
+		    float distanceZ = Input.mousePosition.z - _positionZ;
+		    Vector3 lastPos = Camera.main.ScreenToWorldPoint(new Vector3(distanceX, distanceY, distanceZ));
+		    transform.position = new Vector3(lastPos.x, _startPosition.y, lastPos.z);
+	    }
+    }
+}
