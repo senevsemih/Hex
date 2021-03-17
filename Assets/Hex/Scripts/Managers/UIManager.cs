fe9030829f22c039f39e93f0005f8086d542568f
senevsemih <senevsemih@gmail.com>
2021-03-17 06:52:07 +0300

    +created uımanager

---
+++ Assets/Hex/Scripts/Managers/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using Assets.Hex.Scripts.Managers.Singleton;
+using UnityEngine;
+
+namespace Assets.Hex.Scripts.Managers
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        [SerializeField] private GameObject _nextLevelText;
+
+        public void ActivateGoalText()
+        {
+            StartCoroutine(WaitForActivate());
+        }
+        
+        private IEnumerator WaitForActivate()
+        {
+            yield return new WaitForSeconds(3f);
+            _nextLevelText.SetActive(true);
+        }
+    }
+}
